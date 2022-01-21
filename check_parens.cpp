154f2d0685f940d1fd33c0c115b857ba0b99dab8
Austin <austin.cook.id@gmail.com>
2022-01-21 15:06:49 -0700

    Complete and tested check_parens lab files

--- check_parens.cpp
+++ check_parens.cpp
@@ -1,7 +1,75 @@
 #include <iostream>
+#include <stack>
+
+//Returns bool true if contains one of ({[< false otherwise
+bool IsOpen(char character) {
+    if(character == '(' || character == '{'
+       || character == '[' || character == '<') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//Returns bool true if contains one of )}]> false otherwise
+bool IsClose(char character) {
+    if(character == ')' || character == '}'
+       || character == ']' || character == '>') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//Returns bool true if operators match, false otherwise
+bool AreMatched(char openingChar, char charToCheck) {
+    if(openingChar == '(' && charToCheck == ')') {return true;}
+    else if(openingChar == '{' && charToCheck == '}') {return true;}
+    else if(openingChar == '[' && charToCheck == ']') {return true;}
+    else if(openingChar == '<' && charToCheck == '>') {return true;}
+    else {return false;}
+}
+
 
 int main(int argc, char* argv[]) {
-    std::cout << "This is my program!" << std::endl;
+    bool balanced = true;
+    std::stack<char> charStack;
+
+    std::string userString = argv[1];
+    size_t stringLength = userString.size();
+    int endIndex = stringLength;
+
+    //Process for checking if the string is balanced
+    for(int stringIndex = 0; balanced && stringIndex != endIndex; ++stringIndex) {
+        if (IsOpen(userString.at(stringIndex))) {
+            charStack.push(userString.at(stringIndex));
+        }
+        else if (IsClose(userString.at(stringIndex))) {
+            if(AreMatched(charStack.top(), userString.at(stringIndex))) {
+                charStack.pop();
+            }
+            else {
+                balanced = false;
+            }
+        }
+    }
+
+        if(!charStack.empty()) {
+            balanced = false;
+            //std::cout << std::endl << "The stack is NOT empty"; //DELETEME
+        }
+        else {
+            //std::cout << std::endl << "The stack IS empty"; //DELETEME
+        }
+
+    if(balanced) {
+        std::cout << "true";
+    }
+    else {
+        std::cout << "false";
+    }
 
     return 0;
 }
